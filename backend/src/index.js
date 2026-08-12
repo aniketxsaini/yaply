f2d15b42980897b229fbee26cc5e4333059f4c6c
Aniket <aniketxsaini@gmail.com>
2026-08-12 22:08:48 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import "dotenv/config"
+
+
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.listen(PORT,()=>{
+    console.log(`server is now running on http://localhost:${PORT}`);
+});
+
