98a45d52c7be4b54b71425bbe4c90bb4179fbbd5
Aniket <aniketxsaini@gmail.com>
2026-08-13 23:38:40 +0530

    added docker files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 import express from "express"
 import "dotenv/config"
+
 import {clerkMiddleware} from "@clerk/express"
 import User from "./models/user.model.js"
+
 import {connectDB} from "./lib/db.js"
 import cors from "cors"
+
+import fs from "fs"
+import path from "path"
+const publicDir = path.join(process.cwd(),"public");
 
 const PORT = process.env.PORT;
 const app = express();
@@ -17,6 +23,15 @@ app.use(clerkMiddleware());
 app.get('/health',(req,res)=>{
     res.status(200).json({ok:true});
 })
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err));
+    })
+}
+
 
 app.listen(PORT,()=>{
     connectDB();
