7802aa2017d7aef78460b81df1adddc1db237078
Aniket <aniketxsaini@gmail.com>
2026-08-14 23:26:31 +0530

    update comment for user model in clerk webhook file

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/user.model.js";
-
+// User = Database collection "users" 
 import { verifyWebhook } from "@clerk/backend/webhooks"
 
 const router = express.Router();
